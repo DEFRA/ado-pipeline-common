8a573e68723fdfb393df72a36db15a14e24f7a66
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2023-11-07 12:29:29 +0000

    test original schema

--- templates/powershell/build/ValidateConfigFile.ps1
+++ templates/powershell/build/ValidateConfigFile.ps1
@@ -52,9 +52,7 @@ try {
 
     if (Test-Path $ConfigFilePath -PathType Leaf) {
         if ($ConfigFilePath.EndsWith(".json")) {
-            [string]$json = Get-Content -Raw -Path $ConfigFilePath
-            [string]$jsonschema = Get-Content -Raw -Path $SchemaFilePath
-            Write-Host $jsonschema
+            [string]$json = Get-Content $ConfigFilePath | Out-String
             $result = ( $json | Test-Json -SchemaFile $SchemaFilePath)
         }
         elseif ($ConfigFilePath.EndsWith(".yaml")) {
