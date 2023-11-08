a494b8b40a1860aee2f0347a0b3c0666e60ee76d
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2023-11-08 14:57:41 +0000

    Support for  CommonConfigFile

--- templates/powershell/build/ValidateAndMergeConfigFile.ps1
+++ templates/powershell/build/ValidateAndMergeConfigFile.ps1
@@ -5,21 +5,24 @@ Validate Azure app config file
 Validate Azure app config file
 
 .PARAMETER SchemaFilePath
-Mandatory. Schema file path. 
+Mandatory. Schema file path.
+.PARAMETER CommonConfigFilePath
+Mandatory. Common App Config file path. This config file conents will be merged to env specific config file.
 .PARAMETER ConfigFilePath
 Mandatory. App Config file path. 
 .PARAMETER AppConfigModuleDirectory
 Mandatory. Directory Path of App-Config module
 .EXAMPLE
-.\ValidateConfigFile.ps1  -SchemaFilePath <SchemaFilePath> -ConfigFilePath <ConfigFilePath> -AppConfigModuleDirectory <AppConfigModuleDirectory>
+.\ValidateAndMergeConfigFile.ps1  -SchemaFilePath <SchemaFilePath> -CommonConfigFilePath <CommonConfigFilePath> -ConfigFilePath <ConfigFilePath> -AppConfigModuleDirectory <AppConfigModuleDirectory>
 #> 
 
 [CmdletBinding()]
 param(
     [Parameter(Mandatory)]
     [string] $SchemaFilePath,
-    [Parameter(Mandatory)]
-    [string] $ConfigFilePath,
+    [string] $CommonConfigFilePath,
+    [Parameter(Mandatory)]    
+    [string] $ConfigFilePath,    
     [Parameter(Mandatory)]
     [string] $AppConfigModuleDirectory
 )
@@ -43,17 +46,37 @@ if ($enableDebug) {
 
 Write-Host "${functionName} started at $($startTime.ToString('u'))"
 Write-Debug "${functionName}:SchemaFilePath=$SchemaFilePath"
+Write-Debug "${functionName}:CommonConfigFilePath=$CommonConfigFilePath"
 Write-Debug "${functionName}:ConfigFilePath=$ConfigFilePath"
 Write-Debug "${functionName}:AppConfigModuleDirectory=$AppConfigModuleDirectory"
 
 try {
 
     Import-Module $AppConfigModuleDirectory -Force
+    if (Test-Path $SchemaFilePath -PathType Leaf) {
+        [string]$SchemaFileContent = Get-Content -Raw -Path $SchemaFilePath 
+    }
+
+    if (Test-Path $CommonConfigFilePath -PathType Leaf) {
+        [string]$CommonConfigFileContent = Get-Content -Raw -Path $CommonConfigFilePath 
+        if ($CommonConfigFilePath.EndsWith(".json")) {        
+            $result = ( Test-Json -Json $CommonConfigFileContent -Schema $SchemaFileContent)
+        }
+        elseif ($CommonConfigFilePath.EndsWith(".yaml")) {
+
+            $result = ( Test-Yaml -Yaml $CommonConfigFileContent -Schema $SchemaFileContent)
+        }        
+        if ($result -eq $true) {
+            Write-Host "${functionName} File`t`tPassed validation"
+        }
+        else {
+            Write-Host "${functionName} File`t`tFailed validation"
+            throw [System.IO.InvalidDataException]::new($CommonConfigFilePath) 
+        }
+    }
 
     if (Test-Path $ConfigFilePath -PathType Leaf) {
-        [string]$ConfigFileContent = Get-Content -Raw -Path $ConfigFilePath 
-        [string]$SchemaFileContent = Get-Content -Raw -Path $SchemaFilePath 
-
+        [string]$ConfigFileContent = Get-Content -Raw -Path $ConfigFilePath     
         Write-Debug $ConfigFileContent
         
         if ($ConfigFilePath.EndsWith(".json")) {        
@@ -62,18 +85,25 @@ try {
         elseif ($ConfigFilePath.EndsWith(".yaml")) {
 
             $result = ( Test-Yaml -Yaml $ConfigFileContent -Schema $SchemaFileContent)
-        }
-        else {
-            throw [System.IO.InvalidDataException]::new($ConfigFilePath)            
-        }
+        }        
        
         if ($result -eq $true) {
-            Write-Host "${functionName} JSON File`t`tPassed validation"
+            Write-Host "${functionName} File`t`tPassed validation"
         }
         else {
-            Write-Host "${functionName} JSON File`t`tFailed validation"
+            Write-Host "${functionName} File`t`tFailed validation"
             throw [System.IO.InvalidDataException]::new($ConfigFilePath) 
         }
+    }
+
+    if ($ConfigFilePath.EndsWith(".json")) { 
+        @($CommonConfigFileContent; $ConfigFileContent) | ConvertTo-Json | Out-File $ConfigFilePath
+    }
+    elseif ($ConfigFilePath.EndsWith(".yaml")) {
+        $CommonConfigFileContent  | Out-File -append $ConfigFilePath
+    }
+    else {
+        throw [System.IO.InvalidDataException]::new($ConfigFilePath)            
     }
                
     $exitCode = 0
