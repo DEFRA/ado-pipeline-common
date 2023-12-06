9868316b21e984c2ed46f706f2e9939605fe573b
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2023-12-06 10:57:34 +0000

    test6

--- templates/powershell/build/Extract-CodeVersion.ps1
+++ templates/powershell/build/Extract-CodeVersion.ps1
@@ -117,9 +117,6 @@ try {
     Write-Output "##vso[task.setvariable variable=appVersion;isOutput=true]$appVersion"
     Write-Output "##vso[task.setvariable variable=oldAppVersion;isOutput=true]$oldAppVersion"
     Write-Output "##vso[task.setvariable variable=IsDefaultBranchBuild;isOutput=true]$IsDefaultBranchBuild"
-    $date= (Get-Date).ToString("yyyyMMdd-HHmm")
-    $buildNumber = "$appVersion-$date"
-    Write-Output "##vso[build.updatebuildnumber]$buildNumber"
 }
 catch {
     $exitCode = -2
