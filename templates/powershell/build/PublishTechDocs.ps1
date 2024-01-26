1339f6eecf0f5008e77ca1e05d33507af537deaf
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-01-26 10:52:09 +0000

    activate venv

--- templates/powershell/build/PublishTechDocs.ps1
+++ templates/powershell/build/PublishTechDocs.ps1
@@ -64,8 +64,8 @@ try {
   
     npm install -g @techdocs/cli --loglevel=error
     
-    python -m venv venv
-    source venv/bin/activate
+    python3 -m venv venv
+    ./venv/bin/activate
     pip3 install mkdocs-material --no-warn-script-location
     pip3 install pillow cairosvg --no-warn-script-location
     pip3 install mkdocs-glightbox --no-warn-script-location
