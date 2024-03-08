b274d8096c23da1745dc2e3c9b82b67d9e1fa79f
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-03-08 12:37:53 +0000

    fix

--- templates/powershell/build/HelmLintAndPublish.ps1
+++ templates/powershell/build/HelmLintAndPublish.ps1
@@ -309,7 +309,7 @@ try {
 
                     if ($chartDirectory.DirectoryName.Contains($InfraChartDirName)) {  
                         #Update KeyVault Secret Names in values.yaml file of infrastruture helm chart
-                        if ((Test-Path $chartCacheFilePath -PathType Leaf) -and (-not $KeyVaultVSecretNames.Equals("[]")) { 
+                        if ((Test-Path $chartCacheFilePath -PathType Leaf) -and (-not $KeyVaultVSecretNames.Equals("[]"))) { 
                             Invoke-CommandLine -Command "tar zxf $chartCacheFilePath -C $ChartCachePath"
                             Remove-Item $chartCacheFilePath
                             Update-KVSecretValues -InfraChartHomeDir "$ChartCachePath/$InfraChartDirName" -ServiceName $ServiceName -KeyVaultVSecretNames $KeyVaultVSecretNames
