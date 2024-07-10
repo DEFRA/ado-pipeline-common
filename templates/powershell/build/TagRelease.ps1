b39d0c2e9501952255ae994391127ae30372392d
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-07-10 16:57:40 +0100

    match release to app version

--- templates/powershell/build/TagRelease.ps1
+++ templates/powershell/build/TagRelease.ps1
@@ -77,7 +77,7 @@ try {
         throw $_.Exception
     }
     
-    if ($latestReleaseTag -eq $AppVersion) {
+    if ($latestReleaseTag -match $AppVersion) {
         Write-Host "Release already exists"
         Write-Output "##vso[task.setvariable variable=ReleaseExists]true"
     }
