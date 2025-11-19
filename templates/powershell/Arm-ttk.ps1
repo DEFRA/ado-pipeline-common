1fa07b1a88e634e9af2536e1360764c1a8603647
jeevanthespark <13976923+jeevanthespark@users.noreply.github.com>
2025-11-19 09:59:09 +0000

    Fixed the issue with ARM TTK module download URL in Arm-ttk.ps1

--- templates/powershell/Arm-ttk.ps1
+++ templates/powershell/Arm-ttk.ps1
@@ -36,16 +36,16 @@ $warningtests = @('DependsOn-Best-Practices',
     'DeploymentTemplate-Must-Not-Contain-Hardcoded-Uri',
     'Outputs-Must-Not-Contain-Secrets')
 
-$armTtkModule = Join-Path -Path $PSScriptRoot -ChildPath "arm-template-toolkit" "arm-ttk" "arm-ttk.psd1"
+$armTtkModule = Join-Path -Path $PSScriptRoot -ChildPath "arm-ttk" "arm-ttk" "arm-ttk.psd1"
 Write-Host "Path: $armTtkModule"
 
 if (-not(Test-Path -Path $armTtkModule -PathType Leaf)) {
     try {
         Write-Host "Downloading ARM ttk"
-        $dowloadFilePath = Join-Path -Path $PSScriptRoot -ChildPath "arm-template-toolkit.zip"
-        $extractTookKitPath = Join-Path -Path $PSScriptRoot -ChildPath "arm-template-toolkit"
+        $dowloadFilePath = Join-Path -Path $PSScriptRoot -ChildPath "arm-ttk.zip"
+        $extractTookKitPath = Join-Path -Path $PSScriptRoot -ChildPath "arm-ttk"
 
-        Invoke-WebRequest -Uri 'https://azurequickstartsservice.blob.core.windows.net/ttk/latest/arm-template-toolkit.zip' -OutFile $dowloadFilePath
+        Invoke-WebRequest -Uri 'https://github.com/Azure/arm-ttk/releases/latest/download/arm-ttk.zip' -OutFile $dowloadFilePath
         Expand-Archive -Path $dowloadFilePath -DestinationPath $extractTookKitPath -Force
         Write-Host "ARM ttk extracted"
     }
